8f72c1bc176d32ea3da4db191266acf194476f3e
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-30 13:01:08 -0400

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express'); // importing a CommonJS module
-
+const helmet = require('helmet')
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+const dateLogger = (req, res, next) => {
+  console.log(new Date().toISOString());
+  next()
+}
+
+const logData = (req, res, next) => {
+  console.log(`${req.method}, ${req.url}`)
+  next()
+}
+
+// ==== GLOBAL MIDDLEWARE ====
+server.use(helmet())
 server.use(express.json());
+server.use(dateLogger)
+server.use(logData);
 
 server.use('/api/hubs', hubsRouter);
 
